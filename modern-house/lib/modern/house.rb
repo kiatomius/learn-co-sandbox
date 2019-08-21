b3d6a1ac5b227a75e8652c0d72c3936fd85e74ec
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-21 07:30:52 +0000

    first commit

---
+++ modern-house/lib/modern/house.rb
@@ -0,0 +1,8 @@
+require "modern/house/version"
+
+module Modern
+  module House
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ modern-house/lib/modern/house/version.rb
@@ -0,0 +1,5 @@
+module Modern
+  module House
+    VERSION = "0.1.0"
+  end
+end
